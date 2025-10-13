9bbd2d7de05fb1b91ba42444078feed24050a464
Sikboy79 <custombasewelds@yahoo.com>
2025-10-13 12:30:34 -0600

    home page done

--- src/componetnts/Footer/Footer.jsx
+++ src/componetnts/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './footer.css'
+import youtube_icon from '../../assets/assets/youtube_icon.png'
+import twitter_icon from '../../assets/assets/twitter_icon.png'
+import instagram_icon from '../../assets/assets/instagram_icon.png'
+import facebook_icon from '../../assets/assets/facebook_icon.png'
+
 
 function Footer() {
   return (
     <div className='footer'>
-      
+      <div className='footer-icons'>
+        <img src={facebook_icon} alt=''/>
+        <img src={twitter_icon} alt=''/>
+        <img src={instagram_icon} alt=''/>
+        <img src={youtube_icon} alt=''/>
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Nitices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Imformation</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'> &copy; 1997-2023 Netflix, Inc.</p>
     </div>
   )
 }
--- src/componetnts/Navbar/Navbar.jsx
+++ src/componetnts/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/assets/logo.png'
-import search_icon from '../../assets/assets/search_icon.svg'
-import bell_icon from '../../assets/assets/bell_icon.svg'
-import profile_img from '../../assets/assets/profile_img.png'
-import caret_icon from '../../assets/assets/caret_icon.svg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/assets/logo.png";
+import search_icon from "../../assets/assets/search_icon.svg";
+import bell_icon from "../../assets/assets/bell_icon.svg";
+import profile_img from "../../assets/assets/profile_img.png";
+import caret_icon from "../../assets/assets/caret_icon.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -21,15 +21,19 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-right">
-        <img src={search_icon} alt='' className='icons' />
+        <img src={search_icon} alt="" className="icons" />
         <p>Children</p>
-        <img src={bell_icon} alt='' className='icons' />
-        <div className='navbar-profile'/>
-        <img src={profile_img} alt='' className='profile' />
-        <img src={caret_icon} alt='' />
+        <img src={bell_icon} alt="" className="icons" />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className="profile" />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p> Sign out of Netflix </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

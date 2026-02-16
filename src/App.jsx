0020fb46b1d8f5fdacefebafd972c737593376ce
Sikboy79 <custombasewelds@yahoo.com>
2026-02-16 14:09:10 -0700

    browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import React, { useEffect } from "react";
 import Home from "./pages/Home/Home";
-import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
+import { Routes, Route, useNavigate, BrowserRouter } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const App = () => {
-
+const AppRoutes = () => {
   const navigate = useNavigate();
 
-useEffect(() => {
-  onAuthStateChanged(auth, async (user)=>{
-    if(user){
-      console.log('Logged In');
-      navigate('/')
-    }else{
-      console.log('Logged Out');
-      navigate('/login')
-    }
-  })
-},[])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("Logged In");
+        navigate("/");
+      } else {
+        console.log("Logged Out");
+        navigate("/login");
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [navigate]);
 
   return (
-    <div>
-      <ToastContainer theme='dark' />
+    <>
+      <ToastContainer theme="dark" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
-    </div>
+    </>
+  );
+};
+
+// Wrap AppRoutes in BrowserRouter with basename for GitHub Pages
+const App = () => {
+  return (
+    <BrowserRouter basename="/Movie-clone-fullstack">
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
